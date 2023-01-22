f3483a7bfdc201637d0021b7deaab9e287bddac6
bex-ford <becky782@hotmail.co.uk>
2023-01-22 09:54:16 +0000

    final changes made to JS file

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-let dateDisplayEl = $("#date-display");
-let colorBlock = $(".hour");
+$(document).ready(function () {
+  let dateDisplayEl = $("#date-display");
+  let colorBlock = $(".time-block");
 
-// Displays the current date using moment.js
-function displayDate() {
-  let rightNow = moment().format("dddd MMMM Do");
-  dateDisplayEl.text(rightNow);
- 
-$(".saveBtn").on("click"), function () {
-    let text = $(this).siblings(".description").val();
-localStorage.setItem(text); // set items to local storage within the saveBtn class 
-}}
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
+  // displays the current date using moment.js
+  function displayDate() {
+    let rightNow = moment().format("dddd MMMM Do");
+    dateDisplayEl.text(rightNow);
 
-displayDate();
+    $(".saveBtn").on("click", function () {
+      console.log(this);
+      let text = $(this).siblings(".description").val();
+      let time = $(this).parent().attr("id");
+      console.log(text, time);
+      localStorage.setItem(time, text); // set items to local storage within the saveBtn class
+    });
+  }
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
 
+  displayDate();
 
-// need to change color blocks for past/present/future events 
-function colorChange() {
-  let currentHour = moment().format("h A");
-  for (let index = 0; index < colorBlock.length; index++) {
-    const element = colorBlock[index];
-    if (currentHour === element.id) {
-    
+  function colorChange() {
+    let currentHour = moment().format("h A"); // current hour using moment.js
+    console.log(Number.parseInt(currentHour));
+    for (let index = 0; index < colorBlock.length; index++) {
+      const element = colorBlock[index];
+      if (Number.parseInt(currentHour) === Number.parseInt(element.id)) {
+        console.log(currentHour, element);
+        colorBlock.addClass("present");
+        colorBlock.removeClass("past");
+      } else if (currentHour > Number.parseInt(element.id)) {
+        console.log(currentHour, element, "line 42");
+        colorBlock.addClass("future");
+      } else {
+        colorBlock.addClass("past");
+        colorBlock.removeClass("present");
+      }
     }
   }
-}
-
+  colorChange();
+});
